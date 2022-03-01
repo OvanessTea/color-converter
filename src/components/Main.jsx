eac396ddae8007b8f3eb40b9d5cbbf3acb05df3e
Eduard Suslov <OvanessTea@yandex.com>
2022-03-01 13:44:02 +0300

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,6 @@ function Main() {
 
     useEffect(() => {
         if (/^#([0-9A-F]{3}){1,2}$/i.test(color)) {
-            console.log("hex");
             const normal = color.match(
                 /^#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i
             );
@@ -34,38 +33,61 @@ function Main() {
                 color
             )
         ) {
-            console.log("rgb");
-            const rgbColor = color.split(",");
-            console.log(rgbColor);
-            setConvertedColor(
-                "#" +
-                    (
-                        (1 << 24) +
-                        (rgbColor[0] << 16) +
-                        (rgbColor[1] << 8) +
-                        rgbColor[2]
-                    )
-                        .toString(16)
-                        .slice(1)
-            );
+            const rgbColor = color.split("(")[1].split(")")[0].split(",");
+            // const rgbColor = color.split(",");
+            const b = rgbColor.map(function (x) {
+                x = parseInt(x).toString(16);
+                return x.length === 1 ? "0" + x : x;
+            });
+            setConvertedColor(`#${b[0]}${b[1]}${b[2]}`);
+            // setConvertedColor(
+            //     "#" +
+            //         (
+            //             (1 << 24) +
+            //             (rgbColor[0] << 16) +
+            //             (rgbColor[1] << 8) +
+            //             rgbColor[2]
+            //         )
+            //             .toString(16)
+            //             .slice(1)
+            // );
+        } else {
+            setConvertedColor("");
         }
     }, [color]);
 
     return (
         <main className="_container">
             <div className="converter__container">
-                <div style={{ backgoundColor: color }} className="circle"></div>
+                <div
+                    style={{ backgroundColor: color }}
+                    className="circle"
+                ></div>
                 <div className="converter_body">
                     <h1>
                         Automatic RGB/Hex <br /> color converter
                     </h1>
-                    <p>Enter the color:</p>
+                    <h2>Enter the color:</h2>
                     <input
                         type="text"
                         value={color}
                         onChange={(e) => setColor(e.target.value)}
                     />
-                    <p>{convertedColor}</p>
+                    <h2>Converted value:</h2>
+                    <div className="converted-color_body">
+                        <div className="converted-value">
+                            <span>{convertedColor}</span>
+                        </div>
+                        <button
+                            className="copyToClipboard"
+                            disabled={!convertedColor.length}
+                            onClick={() => {
+                                navigator.clipboard.writeText(convertedColor);
+                            }}
+                        >
+                            Copy
+                        </button>
+                    </div>
                 </div>
             </div>
         </main>
